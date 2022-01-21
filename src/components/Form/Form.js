10f324664561ca706e839b6c10c6b65167275c9c
Henrique Kuwai <henrique.hideki@creditas.com>
2022-01-21 12:00:50 -0300

    finish app functionalities

---
+++ src/components/Form/Form.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { addTodo } from '../../services/todo'
+
+const Form = ({ fetchTodos = () => {} } = {}) => {
+  const [todoText, setTodoText] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addTodo({ text: todoText })
+      .then(() => {
+        fetchTodos()
+        setTodoText('')
+      })
+      .catch(() => toast.error('Error creating todos'))
+  }
+
+  return (
+    <form onSubmit={handleSubmit} style={{ paddingBottom: '20px' }}>
+      <div className="box">
+        <input
+          name="todoText"
+          className="input"
+          type="text"
+          placeholder="Text input"
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+        ></input>
+      </div>
+    </form>
+  )
+}
+
+export { Form }
